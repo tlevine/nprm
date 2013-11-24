e54db50a9e942e597c61da62bca8b62fa474ff9d
Thomas Levine <occurrence@thomaslevine.com>
2013-11-24 20:23:52 +0000

    RJSONIO not loaded

--- test/run.r
+++ test/run.r
@@ -1,11 +1,13 @@
 library(testthat)
-source('../npm.r')
+source('../nprm.r')
 
-expect_that(npm.require('./tau'), equals(2 * pi))
+expect_that('RJSONIO' %in% loadedNamespaces(), is_false())
+
+expect_that(nprm.require('./tau'), equals(2 * pi))
 expect_that(exists('tau'), is_false())
 
-expect_that(npm.require('tau'), equals(2 * pi))
+expect_that(nprm.require('tau'), equals(2 * pi))
 expect_that(exists('tau'), is_false())
 
-expect_that(is.function(npm.require('treasury.io')), is_true())
+expect_that(is.function(nprm.require('treasury.io')), is_true())
 expect_that(exists('getURL'), is_false())
