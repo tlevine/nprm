084d92e9e3bd6aba01df23c19bbfca2e7ad28862
Thomas Levine <occurrence@thomaslevine.com>
2013-11-24 20:01:03 +0000

    another test

--- test/run.r
+++ test/run.r
@@ -2,7 +2,10 @@ library(testthat)
 source('../npm.r')
 
 expect_that(npm.require('./tau'), equals(2 * pi))
-expect_that(exists('tau'), equals(FALSE))
+expect_that(exists('tau'), is_false())
 
 expect_that(npm.require('tau'), equals(2 * pi))
-expect_that(exists('tau'), equals(FALSE))
+expect_that(exists('tau'), is_false())
+
+expect_that(is.function(npm.require('treasury.io')), is_true())
+expect_that(exists('getURL'), is_false())
